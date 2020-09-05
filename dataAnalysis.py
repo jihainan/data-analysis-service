f2641e0d26c9d528f42ac502b5c87f8755de1c30
jihainan <alwaysbegoing@163.com>
2020-09-05 10:34:30 +0800

    comment code

--- dataAnalysis.py
+++ dataAnalysis.py
@@ -3,33 +3,36 @@ import os
 import random
 import pylab
 
-
+# MQTT broker 地址
 HOST = "192.168.31.142"
 PORT = 1883
-x= 0
-p=0.5
-r=0.4
-cycle=0
-lastMsg=0
+x = 0
+p = 0.5
+r = 0.4
+cycle = 0
+lastMsg = 0
 
-
-angle=[]
-angle_filter=[]
+angle = []
+angle_filter = []
 
 
 def kalman(newMeasure):
     global x
     global p
     global r
-    k= p/(p+r)
-    p=(1-k)*p
-    x= x+ k*(newMeasure-x)
+    k = p / (p + r)
+    p = (1 - k) * p
+    x = x + k * (newMeasure - x)
     return x
 
-def plotfig(a,b):
+
+# *****************************************
+# draw feature with measurement/estimation
+# *****************************************
+def plotfig(a, b):
     pylab.figure()
-    pylab.plot(a,'k*',label='measurement')
-    pylab.plot(b,'b^',label='estimation')
+    pylab.plot(a, 'k*', label='measurement')
+    pylab.plot(b, 'b^', label='estimation')
     pylab.title('Angle of the wind')
     pylab.legend()
     pylab.xlabel('time')
@@ -37,50 +40,51 @@ def plotfig(a,b):
     pylab.show()
 
 
+# *****************************************
+# MQTT message callback for receive measurement data
+# *****************************************
 def on_message_callback(client, userdata, message):
-
-    msg= str(message.payload)
-    msg= msg[2:-1]
-
+    msg = str(message.payload)
+    msg = msg[2:-1]
 
     # send DataTopic---one angle
     if msg.isdigit():
 
         print("====================================")
-        print("Get data "+msg)
+        print("Get data " + msg)
 
-        #initialize x
+        # initialize x
         global cycle
         global x
         global p
         global lastMsg
 
-        if cycle==0:
-            x=int(msg)
-        cycle+=1
+        if cycle == 0:
+            x = int(msg)
+        cycle += 1
 
-        if abs(int(msg)-lastMsg)>10:
-            x= int(msg)
-            p= 0.5
-        lastMsg=int(msg)
+        if abs(int(msg) - lastMsg) > 10:
+            x = int(msg)
+            p = 0.5
+        lastMsg = int(msg)
 
-
-        result= int(kalman(int(msg)))
+        result = int(kalman(int(msg)))
         angle.append(int(msg))
         angle_filter.append(result)
-        if len(angle_filter)==100:
-            plotfig(angle,angle_filter)
-
-        command = "mosquitto_pub -h 192.168.31.142 -t YanniTopic  -m "+ str(result)+str(result)
-        print("After Kalman, send terminal "+ str(result))
+        if len(angle_filter) == 100:
+            plotfig(angle, angle_filter)
+        # publish message to specific topic
+        command = "mosquitto_pub -h 192.168.31.142 -t YanniTopic  -m " + str(result) + str(result)
+        print("After Kalman, send terminal " + str(result))
         print("====================================")
         print("")
         os.system(command)
 
-
-
+# *****************************************
+# connected to MQTT broker
+# *****************************************
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+    print("Connected with result code " + str(rc))
     client.subscribe("YanniiTopic")
 
 
@@ -94,5 +98,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
